4046625ae612e14033de80fe2530060f1dfb5a7a
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-22 16:00:53 +0530

    new changes

--- Each Name With Greetings/script.js
+++ Each Name With Greetings/script.js
@@ -1,9 +1,15 @@
-let persons = ["john","michael", "alan", "james"];
-let result=""
-for(let i =0; i<persons.length; i++){
-result += `Hey welocme`+" " +persons[i] +" <br>";
-}
+const persons = {
+    name1:"john",
+    name2:"michael",
+    name3:"alan",
+    name4:"james"
+};
+let result = `Hey welocme   ${persons.name1}!`+" <br> " 
++`Hey welocme   ${persons.name2}!`+"<br>"
++`Hey welocme   ${persons.name3}!`+"<br>"
++`Hey welocme   ${persons.name4}!`+"<br>"
+
+
+
 document.getElementById("para").innerHTML= result;
 
-//console 
- console.log(result);

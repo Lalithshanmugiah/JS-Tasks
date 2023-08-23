51a171a5658425ef91ec7be4b690c18ce904f693
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-23 17:55:41 +0530

    new changes

--- revalent day/script.js
+++ revalent day/script.js
@@ -1,31 +1,29 @@
 function myFunction() {
 
-    var days = document.getElementById("Input").value;
-  
-    switch(days) {
-      case "1":
-      text = "monday";
-      break;
-      case "2":
-      text = "tuesday";
-      break;
-      case "3":
-      text = "wednesday";
-      break;
-      case "4":
-      text = "thursday";
-      break;
-      case "5":
-      text = "friday";
-      break;
-      case "6":
-      text = "saturday";
-      break;
-      case "7":
-      text = "sunday ";
-      break;
-      default:
-      text = "enter valid number";
-    }
-    document.getElementById("para").innerHTML = text;
+  let input = document.getElementById("input").value;
+  let output = document.getElementById("output");
+  //empty value should shows enter valid number 
+  if (input === "") {
+      output.innerText = "Enter Valid Number";
+      return;
   }
+  //if any negative number or else equal to zero shows enter positive number
+  if (!/^\d+(\.\d+)?$/.test(input) || parseFloat(input)<=0) {
+      output.innerText = "Enter Positive Number ";
+      return;
+  }
+  //if any decimal number entered its rounded to nearest number
+  let Rounded = Math.round(parseFloat(input));
+  //if enter number were greater than 7 and less than 1 mean its shows enter number between 1 to 7
+  if (Rounded < 1 || Rounded > 7) {
+      output.innerText = "Enter Number Between 1 to 7";
+      return;
+  }
+  // result of revalent days
+  let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  let relevantDay = days[Rounded - 1];
+  output.innerText = relevantDay;
+
+
+}
+

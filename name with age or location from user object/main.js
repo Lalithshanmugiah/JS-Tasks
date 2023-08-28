db83cde0ac696f95fb796d2edf685beb97ac5da3
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-28 16:42:27 +0530

    name with age or location from user object

--- name with age or location from user object/main.js
+++ name with age or location from user object/main.js
@@ -8,30 +8,18 @@ const user ={
         this.lastName = document.getElementById('lastName').value;
         this.age = document.getElementById('age').value;
         this.location = document.getElementById('location').value;
-        this.displayOutput();
+        
     },
-    displayOutput:function() {
-        const outputDiv = document.getElementById('output');
-        const ageButton = document.createElement('button');
-        ageButton.textContent='Age';
-        ageButton.addEventListener('click', () => this.displayAge());
-
-        const locationButton = document.createElement('button');
-        locationButton.textContent='Location';
-        locationButton.addEventListener('click',() => this.displayLocation());
-
-        outputDiv.innerHTML =''
-        outputDiv.appendChild(ageButton);
-        outputDiv.appendChild(locationButton);     
-    },
-    displayAge: function() {
-        const outputDiv = document.getElementById('output');
-        outputDiv.textContent=`${this.firstName}${this.lastName}${this.age}`;
-    },
-    displayLocation: function(){
-        const outputDiv = document.getElementById('output');
-        outputDiv.textContent=`${this.firstName}${this.lastName}${this.location}`;
-    } 
+    displayoutput: function(type) {
+        const output = `${this.firstName} ${this.lastName} ${
+            type === 'age' ? this.age : this.location
+        }`;
+        document.getElementById('output').innerHTML = output;
+        
+    }
 };
+document.getElementById('output').innerHTML = `
+            <button onclick="user.displayoutput('age')">Age Button</button>
+            <button onclick="user.displayoutput('location')">Location Button</button>`;
 
 document.getElementById('Button').addEventListener('click', () => user.storeInformation());

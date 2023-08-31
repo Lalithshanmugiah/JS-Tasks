242488510de2e5ec0dda49bf9dfd1c7ae2957a1a
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-31 17:38:32 +0530

    gender info

--- Gender field/script.js
+++ Gender field/script.js
@@ -2,13 +2,13 @@
 const storeButton = document.getElementById('storeButton');
 const name = document.getElementById('name');
 const gender = document.getElementById('gender');
-const maleNames= document.getElementById('maleNames');
+const maleNames = document.getElementById('maleNames');
 const femaleNames = document.getElementById('femaleNames');
 const otherNames = document.getElementById('otherNames');
 
 storeButton.addEventListener('click', function() {
-  const name = name.value;
-  const gender = gender.value;
+  const name = nameInput.value;
+  const gender = genderInput.value;
 
   const nameItem = document.createElement('li');
   nameItem.textContent = name;
@@ -22,6 +22,6 @@ storeButton.addEventListener('click', function() {
   }
 
   
-  nameInput.value = '';
-  genderInput.value = 'male'; 
+  name.value = '';
+  gender.value = 'male'; 
 });
